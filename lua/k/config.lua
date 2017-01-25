65f63741488320f89e546cae7a72f24a3ea09416
pepe-roni <ahde2k@gmail.com>
2017-01-25 15:30:44 -0800

    fvhvbfdj

---
+++ lua/k/config.lua
@@ -0,0 +1,11 @@
+-- config.lua
+
+application =
+{
+	content =
+	{
+		width = 750,
+		height = 1200,
+		scale = "zoomEven" 
+	},
+}
--- lua/k/main.lua
+++ lua/k/main.lua
@@ -1,6 +1,16 @@
-print("hello")
-display.setStatusBar(display.StatusBar)
-display.newImage("oie_transparent.png",365,400)
-local function meme( int x )
-	-- body
+display.setStatusBar(display.HiddenStatusBar)
+local degrees = 0
+
+local background = display.newImage("images/back_1.jpg")
+background.x =display.contentCenterX
+background.y = display.contentCenterY
+-- 400x, 600y
+local logo = display.newImage("images/onlyLogo.png")
+logo:rotate(degrees)
+logo.x = display.contentCenterX
+logo.y = display.contentCenterY*0.5
+rot()
+
+local function rot()
+	degrees = degrees + 1
 end
