3c487f8576802616db78c29645bc026a2588df0b
pepe-roni <ahde2k@gmail.com>
2017-01-30 15:32:57 -0800

    dfvjf

--- lua/k/main.lua
+++ lua/k/main.lua
@@ -1,16 +1,29 @@
 display.setStatusBar(display.HiddenStatusBar)
-local degrees = 0
 
 local background = display.newImage("images/back_1.jpg")
 background.x =display.contentCenterX
 background.y = display.contentCenterY
--- 400x, 600y
-local logo = display.newImage("images/onlyLogo.png")
-logo:rotate(degrees)
-logo.x = display.contentCenterX
-logo.y = display.contentCenterY*0.5
-rot()
 
-local function rot()
-	degrees = degrees + 1
+local logo = display.newImage("images/onlyLogo.png")
+logo.x = display.contentCenterX
+logo.y = display.contentCenterY - 300
+
+
+local function fade()
+	transition.to(background,{time = 2000, alpha = 0})
+	transition.to(logo, {time = 500, alpha = 0})
+--	transition.to(background,{time = 4000, display.newImage("images/back_2.jpeg")})
 end
+Runtime:addEventListener("tap", fade)
+
+local function meme()
+	local back2 = display.newImage("images/back_2.jpeg")
+	back2.alpha = 0
+	back2.x =display.contentCenterX
+	back2.y = display.contentCenterY
+	transition.to(back2, {time = 3000, alpha = 1})
+end
+Runtime:addEventListener("tap", meme)
+transition.from(logo, {time = 1000, alpha = 0})
+
+
