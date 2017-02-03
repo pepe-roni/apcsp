aa3b13b986f62d30699e0ddd8703ac8c59a5d3f9
pepe-roni <ahde2k@gmail.com>
2017-02-03 15:30:38 -0800

    hlfdbnfdbv

--- lua/m8/main.lua
+++ lua/m8/main.lua
@@ -4,7 +4,13 @@ local preRec = display.newImage("/image/cyan_texture_by_ladyoftheapocalypse-d9bt
 	preRec.xScale = 1.2
 	preRec.yScale = 1.2
 	preRec.alpha = 0.8 
+local pretext = display.newText("Click to display stuff.",
+	display.contentCenterX,display.contentCenterY,"/fonts/Roboto-LightItalic.ttf" )
+	pretext:scale(1.2,1.2)
 
+	pretext.x = display.contentCenterX
+	pretext.y = display.contentCenterY
+	pretext.alpha = 1 
 local background = display.newImage("/image/ZsfUJx8.jpg")
 	background.x = display.contentCenterX
 	background.y = display.contentCenterY
@@ -17,11 +23,12 @@ local ball = display.newImage("/image/glassorb1.png")
 	ball.alpha = 0
 
 local function afterclick()
-	transition.to(preRec, {time = 750, alpha = 0})
+	transition.to(pretext, {time = 300, alpha =0})
+	transition.to(preRec, {time = 500, alpha = 0})
 	transition.to(background, {time = 1500, alpha = 1})
 	transition.to(ball, {time = 1500, alpha = 0.75 })
 end
-Runtime:addEventListener("tap", afterclick)
+preRec:addEventListener("tap", afterclick)
 
 
 
