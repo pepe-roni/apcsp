a8ef30c481f4421c4e1af3ec420486b076df0921
pepe-roni <ahde2k@gmail.com>
2017-02-06 15:33:27 -0800

    jrnjkgfejk

--- lua/m8/main.lua
+++ lua/m8/main.lua
@@ -4,6 +4,7 @@ local preRec = display.newImage("/image/cyan_texture_by_ladyoftheapocalypse-d9bt
 	preRec.xScale = 1.2
 	preRec.yScale = 1.2
 	preRec.alpha = 0.8 
+
 local pretext = display.newText("Click to display stuff.",
 	display.contentCenterX,display.contentCenterY,"/fonts/Roboto-LightItalic.ttf" )
 	pretext:scale(1.2,1.2)
@@ -21,15 +22,43 @@ local ball = display.newImage("/image/glassorb1.png")
 	ball.x = display.contentCenterX
 	ball.y = display.contentCenterY - 50
 	ball.alpha = 0
+local meme = display.newText(" ",display.contentCenterX,display.contentCenterY-50,"/fonts/Roboto-LightItalic.ttf")
 
 local function afterclick()
 	transition.to(pretext, {time = 300, alpha =0})
 	transition.to(preRec, {time = 500, alpha = 0})
 	transition.to(background, {time = 1500, alpha = 1})
 	transition.to(ball, {time = 1500, alpha = 0.75 })
+	local stuff = {"It is certain","It is decidedly so",
+"Without a doubt",
+"Yes definitely",
+"You may rely on it",
+"As I see it, yes",
+"Most likely",
+"Outlook good",
+"Yes",
+"Signs point to yes",
+"Reply hazy try again",
+"Ask again later",
+"Better not tell you now",
+"Cannot predict now",
+"Concentrate and ask again",
+"Dont count on it",
+"My reply is no",
+"My sources say no",
+"Outlook not so good",
+"Very doubtful"}
+	meme.alpha = 1
+	meme:setFillColor(1,0.45,0.3)
+	meme.fontSize = 25
+	meme.text = stuff[math.random(1,20)]
+
 end
-preRec:addEventListener("tap", afterclick)
 
+Runtime:addEventListener("tap", afterclick)
 
+local function change()
+	transition.to(meme, {time = 1000, alpha = 1})
+end
 
-
+Runtime:addEventListener("tap", change)
