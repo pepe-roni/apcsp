5f553c8d340b53ef01d2f236f165865a450e3e8c
pepe-roni <ahde2k@gmail.com>
2017-02-08 15:29:42 -0800

    fdjhvb

---
+++ lua/SimplePhysics/main.lua
@@ -0,0 +1,35 @@
+local physics = require ("physics")
+physics.start()
+local preRec = display.newImage("/image/cyan_texture_by_ladyoftheapocalypse-d9btc9w.jpg")
+	preRec.x = display.contentCenterX
+	preRec.y = display.contentCenterY
+	preRec.xScale = 1.2
+	preRec.yScale = 1.2
+	preRec.alpha = 0.8 
+local pretext = display.newText("Click to display stuff.",
+	display.contentCenterX,display.contentCenterY,"/fonts/Roboto-LightItalic.ttf" )
+	pretext:scale(1.2,1.2)
+	pretext.x = display.contentCenterX
+	pretext.y = display.contentCenterY
+	pretext.alpha = 1 
+
+local background = display.newImage("/image/ZsfUJx8.jpg")
+	background.x = display.contentCenterX
+	background.y = display.contentCenterY
+	background.alpha = 0
+	background:scale(0.2,0.2)
+local ball = display.newImage("/image/glassorb1.png")
+	ball.x = display.contentCenterX
+	ball.y = display.contentCenterY - 50
+	ball:scale(0.2,0.2)
+	ball.alpha = 0
+
+local function afterclick()
+	transition.to(pretext, {time = 300, alpha =0})
+	transition.to(preRec, {time = 500, alpha = 0})
+	transition.to(background, {time = 1500, alpha = 0.75})
+	transition.to(ball, {time = 1500, alpha = 0.75 })
+end
+physics.addBody(ball, {density=3.0, friction = 1.0, bounce = 0.2})
+
+preRec:addEventListener("tap", afterclick)
