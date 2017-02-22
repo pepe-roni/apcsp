84d4176652861620b7f45240038805b08c84304e
pepe-roni <ahde2k@gmail.com>
2017-02-22 15:06:51 -0800

    fdhjvfb

--- lua/sidesc4roller/main.lua
+++ lua/sidesc4roller/main.lua
@@ -1,5 +1,28 @@
 local physics = require ("physics")
 physics.start()
+local options = 
+{
+	width = 175,
+	height = 175,
+	numFrames = 10,
+}
+
+local squid  = graphics.newImageSheet("/image/act_enm_oct_flm.png", options)
+local sequenceData = 
+{
+	{
+		name = "swim",
+		start = 1,
+		count = 4,
+		time=500,
+	},
+}
+
+local sprite = display.newSprite(squid,sequenceData)
+sprite.x = 240
+sprite.alpha = 0
+sprite.y =270
+sprite:play()
 
 local preRec = display.newImage("/image/cyan_texture_by_ladyoftheapocalypse-d9btc9w.jpg")
 	preRec.x = display.contentCenterX
@@ -11,45 +34,39 @@ local preRec = display.newImage("/image/cyan_texture_by_ladyoftheapocalypse-d9bt
 local pretext = display.newText("Yellow = Draw Ball, Blue, Pink adjust grav",
 	display.contentCenterX, display.contentCenterY,"/fonts/Roboto-LightItalic.ttf")
 	pretext.alpha = 1 
-
-local background = display.newImage("/image/kek.png")
+--no comment
+local background = display.newImage("/image/1.png")
 	background.x = 300
 	background.y = display.contentCenterY
 	background.alpha = 0
 	background:scale(1.5,1.5)
 
 
-local background2 = display.newImage("/image/kek.png")
-	background2.x = 900
+local background2 = display.newImage("/image/1.png")
+	background2.x = -300
 	background2.y = display.contentCenterY
 	background2.alpha = 0
 	background2:scale(1.5,1.5)
-
-
-local gx, gy = physics.getGravity()
 
 local function afterclick()
 	transition.to(pretext, {time = 300, alpha =0})
 	transition.to(preRec, {time = 500, alpha = 0})
 	transition.to(background, {time = 1500, alpha = 0.75})
-	transition.to(background2, {time = 1500, alpha = 0.75})
+	transition.to(background2, {time = 1500, alpha = 1})
+	transition.to(sprite, {time = 1500, alpha = 1})
 end
 
+
 local function left()
-	background.x = background.x - 1
-	if(background.x<-600) then
-		background.x = 600
+	background.x = background.x + 1.4
+	if(background.x> 800) then
+		background.x = -300
 	end
-	background2.x = background2.x - 1
-	if(background2.x<-600) then
-		background2.x=600
+	background2.x = background2.x + 1.2
+	if(background2.x> 800) then
+		background2.x=-300
 	end
 end
 
 timer.performWithDelay(1, left, -1)
-
-local function reset()
-	ball.y = 100
-end
-
 preRec:addEventListener("tap", afterclick)
