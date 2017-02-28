6d7eba9349968a44f9580a57a7c66456e062b831
pepe-roni <ahde2k@gmail.com>
2017-02-28 15:32:40 -0800

    fdjhbvbds

--- lua/sidesc4roller/main.lua
+++ lua/sidesc4roller/main.lua
@@ -59,7 +59,7 @@ local background2 = display.newImage("/image/1.png")
 	background2.alpha = 0
 	background2:scale(1.5,1.5)
 
-local pretext = display.newText("Yellow = Draw Ball, Blue, Pink adjust grav",
+local pretext = display.newText("Dont get hit by the torpedo! \n Click to accelerate! \n Torpedo distance is on top along with score \n Click Score to Pause \n Click yellow box to reset when ded",
 	display.contentCenterX, display.contentCenterY,"/fonts/Roboto-LightItalic.ttf")
 	pretext.alpha = 1 
 --no comment
@@ -84,43 +84,82 @@ local scoreValue = 0
 local score = display.newText(scoreValue,
 	display.contentCenterX+230, display.contentCenterY-100,"/fonts/Roboto-LightItalic.ttf")
 
-
-local function left()
-	background.x = background.x - 30
-	if(background.x< -300) then
-		background.x = 800
-	end
-	background2.x = background2.x - 30
-	if(background2.x< -300) then
-		background2.x= 800
-	end
-
-	if(torpedo.x+35<sprite.x+35 and torpedo.x-35<sprite.x-35) then
-		endgame=true
-	end
-	if(endgame == false) then
-		torpedo.y = torpedo.y + 10.35
-
-		if(torpedo.y > 450) then
-			scoreValue = scoreValue + 1
-			score.text = scoreValue
-			torpedo.x = math.random(1,450)
-			torpedo.y = math.random(-800,-100)
-		end
-	end
-
-end
+local goodgame = false
 
 local function kek()
 	torpedo.y = -800
 end
 
 local function ryzen()
+	if(endgame) then
+		sprite:setLinearVelocity(0,0)
+	else
 	sprite:applyLinearImpulse(77.5,0,sprite.x, sprite.y)
 	ti = 200
+	
+	end
+end
+
+local function reset()
+	scoreValue = 0
+	sprite.x = 0
+	torpedo.y = -950
+	endgame = false
+	goodgame = false
+end 
+
+local meters = display.newText(0,
+	display.contentCenterX-150, display.contentCenterY-100,"/fonts/Roboto-LightItalic.ttf")
+
+local function pause()
+	if(endgame) then
+	 endgame = false
+	else
+		sprite:setLinearVelocity(0,0)
+		physics.setGravity(0,9)
+		endgame = true
+	end
+end
+
+local function left()
+	if((torpedo.x <= sprite.x+65) and (torpedo.x >= sprite.x-65) and (torpedo.y >= 175) and torpedo.y <= 225) then
+		endgame = true
+		goodgame = true
+	end
+
+	meters.text = torpedo.y
+
+print(torpedo.y)
+	if(endgame == false) then
+		physics.setGravity(-10.35,9)
+		background.x = background.x - 30
+		if(background.x< -300) then
+			background.x = 800
+		end
+		background2.x = background2.x - 30
+		if(background2.x< -300) then
+			background2.x= 800
+		end
+		torpedo.y = torpedo.y + 15
+
+		if(torpedo.y > 450) then
+			scoreValue = scoreValue + 1
+			score.text = scoreValue
+			torpedo.x = sprite.x
+			torpedo.y = math.random(-400,-100)
+		end
+	end
+	if(goodgame) then
+		local button = display.newRect(225,50,50,30)
+		button:setFillColor(1,1,0.5)
+		button:addEventListener("tap", reset)
+		physics.setGravity(0,9)
+		sprite:setLinearVelocity(0,0)
+	end
 end
 
 timer.performWithDelay(1, left, -1)
 Runtime:addEventListener("touch", ryzen)
 torpedo:addEventListener("collision" , kek )
 preRec:addEventListener("tap", afterclick)
+score:addEventListener("tap", pause)
